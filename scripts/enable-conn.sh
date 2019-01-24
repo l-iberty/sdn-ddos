d58de0579385941a35d264b571a76f5c2aa4615f
l-iberty <2593093242@qq.com>
2019-01-24 13:15:52 +0800

    update

--- scripts/enable-conn.sh
+++ scripts/enable-conn.sh
@@ -1,5 +1,7 @@
-URL=http://localhost:8080/wm/firewall/rules/json
+URL_FIREWALL_ENABLE=http://localhost:8080/wm/firewall/module/enable/json
+URL_FIREWALL_RULES=http://localhost:8080/wm/firewall/rules/json
 
-curl -X POST -d  '{"src-ip":"*","dst-ip":"*","dl-type":"ARP"}' $URL
-curl -X POST -d  '{"src-ip":"*","dst-ip":"*","nw-proto":"ICMP"}' $URL
-curl -X POST -d  '{"src-ip":"*","dst-ip":"*","tp-src":"*","tp-dst":"*","nw-proto":"TCP"}' $URL
+curl -X PUT ${URL_FIREWALL_ENABLE}
+curl -X POST -d  '{"src-ip":"*","dst-ip":"*","dl-type":"ARP"}' ${URL_FIREWALL_RULES}
+curl -X POST -d  '{"src-ip":"*","dst-ip":"*","nw-proto":"ICMP"}' ${URL_FIREWALL_RULES}
+curl -X POST -d  '{"src-ip":"*","dst-ip":"*","tp-src":"*","tp-dst":"*","nw-proto":"TCP"}' ${URL_FIREWALL_RULES}
