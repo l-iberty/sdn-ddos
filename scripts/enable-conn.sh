af8f19fa816997940acc073265c044b7b1789038
l-iberty <2593093242@qq.com>
2019-03-11 13:05:26 +0800

    udpate

--- scripts/enable-conn.sh
+++ scripts/enable-conn.sh
@@ -2,6 +2,6 @@ URL_FIREWALL_ENABLE=http://localhost:8080/wm/firewall/module/enable/json
 URL_FIREWALL_RULES=http://localhost:8080/wm/firewall/rules/json
 
 curl -X PUT ${URL_FIREWALL_ENABLE}
-curl -X POST -d  '{"src-ip":"*","dst-ip":"*","dl-type":"ARP"}' ${URL_FIREWALL_RULES}
-curl -X POST -d  '{"src-ip":"*","dst-ip":"*","nw-proto":"ICMP"}' ${URL_FIREWALL_RULES}
-curl -X POST -d  '{"src-ip":"*","dst-ip":"*","tp-src":"*","tp-dst":"*","nw-proto":"TCP"}' ${URL_FIREWALL_RULES}
+curl -X POST -d  '{"src-ip":"*","dst-ip":"*","dl-type":"ARP","priority":"10"}' ${URL_FIREWALL_RULES}
+curl -X POST -d  '{"src-ip":"*","dst-ip":"*","nw-proto":"ICMP","priority":"10"}' ${URL_FIREWALL_RULES}
+curl -X POST -d  '{"src-ip":"*","dst-ip":"*","tp-src":"*","tp-dst":"*","nw-proto":"TCP","priority":"10"}' ${URL_FIREWALL_RULES}
