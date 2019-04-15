1b4e6348a68129e4bcac96405e7c18a7abefd941
l-iberty <2593093242@qq.com>
2019-04-15 18:45:43 +0800

    update

--- scripts/mytopo.py
+++ scripts/mytopo.py
@@ -7,23 +7,14 @@ class MyTopo( Topo ):
 	def __init__( self ):
 		"Create custom topo."
 		"""
-             [h2]          [h5]          [h8]
-         [h1] |  [h3]  [h4] |  [h6]  [h7] |  [h9]
-           \  |  /       \  |  /       \  |  /
-            \ | /         \ | /         \ | /
-             [s1]----------[s2]----------[s3]
-                            |
-                            |
-                           [s4]
-                         /      \
-                        /        \
-                       /          \
-                    [s5]          [s6]
-                   / | \          / | \
-                  /  |  \        /  |  \
-                 /   |   \   [h13]  |   [h15]
-             [h10]   |   [h12]    [h14]
-                   [h11]
+                        [h1]
+                         |
+                        [s1]
+                       /    \
+                      /      \
+                    [s2]     [s3]
+                    /  \     /   \
+                  [h2] [h3] [h4] [h5]
 		"""
 
 		Topo.__init__(self)
@@ -33,43 +24,18 @@ class MyTopo( Topo ):
 		h3 = self.addHost( 'h3' )	
 		h4 = self.addHost( 'h4' )	
 		h5 = self.addHost( 'h5' )	
-		h6 = self.addHost( 'h6' )	
-		h7 = self.addHost( 'h7' )	
-		h8 = self.addHost( 'h8' )	
-		h9 = self.addHost( 'h9' )	
-		h10 = self.addHost( 'h10' )	
-		h11 = self.addHost( 'h11' )	
-		h12 = self.addHost( 'h12' )	
-		h13 = self.addHost( 'h13' )	
-		h14 = self.addHost( 'h14' )	
-		h15 = self.addHost( 'h15' )	
 		# add switches
 		s1 = self.addSwitch( 's1' )
 		s2 = self.addSwitch( 's2' )
 		s3 = self.addSwitch( 's3' )
-		s4 = self.addSwitch( 's4' )
-		s5 = self.addSwitch( 's5' )
-		s6 = self.addSwitch( 's6' )
 		# add links
 		self.addLink( s1, s2 )
-		self.addLink( s2, s3 )
-		self.addLink( s2, s4 )
-		self.addLink( s4, s5 )
-		self.addLink( s4, s6 )
+		self.addLink( s1, s3 )
 		self.addLink( s1, h1 )
-		self.addLink( s1, h2 )
-		self.addLink( s1, h3 )
-		self.addLink( s2, h4 )
-		self.addLink( s2, h5 )
-		self.addLink( s2, h6 )
-		self.addLink( s3, h7 )
-		self.addLink( s3, h8 )
-		self.addLink( s3, h9 )
-		self.addLink( s5, h10 )
-		self.addLink( s5, h11 )
-		self.addLink( s5, h12 )
-		self.addLink( s6, h13 )
-		self.addLink( s6, h14 )
-		self.addLink( s6, h15 )
+		self.addLink( s2, h2 )
+		self.addLink( s2, h3 )
+		self.addLink( s3, h4 )
+		self.addLink( s3, h5 )
 	
 topos = { 'mytopo': ( lambda: MyTopo() ) }
+
